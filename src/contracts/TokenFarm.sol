459385af942bba0c4d0f06fcab2d58677ad5b9a1
guramritpalsaggu <guramritpalsaggu@gmail.com>
2021-06-02 16:45:52 +0530

    stake tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,8 +8,30 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    // 1. Stakes Tokens (deposit)
+    function stakeTokens(uint _amount) public {
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
+    }
+
+    // 2. Unstacking tokens(withdraw)
+
+    // 3. Issuing tokens
+
 }
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -49,4 +49,30 @@ contract('TokenFarm', ([owner, investor]) => {
         let balance = await dappToken.balanceOf(tokenFarm.address)
         assert.equal(balance.toString(), tokens('1000000'))
     })
+
+    describe('Farming tokens', async () => {
+        it('rewards investors for staking mDai tokens', async() => {
+            let result
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('100'), "Investor Mock DAI wallet balance correct before staking")
+
+            await daiToken.approve(tokenFarm.address, tokens('100'), { from: investor})
+            await tokenFarm.stakeTokens(tokens('100'), { from: investor})
+
+            // Check staking result
+            result = await daiToken.balanceOf(investor)
+            assert.equal(result.toString(), tokens('0'), 'investor Mock DAI wallet balance correct after staking')
+
+            result = await daiToken.balanceOf(tokenFarm.address)
+            assert.equal(result.toString(), tokens('100'), 'Token Farm Mock DAI balance correct after staking')
+
+            result = await tokenFarm.stakingBalance(investor)
+            assert.equal(result.toString(), tokens('100'), 'investor staking balance correct after staking')
+            
+            result = await tokenFarm.isStaking(investor)
+            assert.equal(result.toString(), 'true', 'investor staking status correct after staking')
+
+            
+        })
+    })
 })
